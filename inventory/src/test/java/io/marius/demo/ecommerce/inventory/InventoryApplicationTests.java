d5cbd0312d0bf2957b58b0afa70de237eb84bfd4
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-04-07 11:22:15 +0300

    initial commit;

---
+++ inventory/src/test/java/io/marius/demo/ecommerce/inventory/InventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package io.marius.demo.ecommerce.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
