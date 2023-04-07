e015c058b784c603343ab9cfb63e3ef7fd183925
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-04-07 11:30:32 +0300

    adding initial db structure;

---
+++ inventory/src/main/resources/db/migration/V1/V2__insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO PUBLIC.inventory_item (NAME,
+                                   PRICE,
+                                   QUANTITY)
+VALUES ('Chair', 100.99, 10),
+       ('Table', 500.99, 5),
+       ('Sofa', 1200.99, 2);
+
