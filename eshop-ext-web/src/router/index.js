50683fb4b7628d7b40754cba77c0896f1e8c2621
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-05-04 09:42:47 +0300

    fixing navigation issues;

--- eshop-ext-web/src/router/index.js
+++ eshop-ext-web/src/router/index.js
@@ -1,44 +1,24 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Inventory from "@/views/tabs/Inventory.vue";
-import Products from "@/views/tabs/Product.vue";
 import HomeTabs from "@/views/Home.vue";
 import Login from "@/views/Login.vue";
 import {useAppStore} from '@/store/app'
+import ProductList from "@/views/tabs/product/ProductList.vue";
 
-const routes = [
-  {
-    path: '/',
-    component: () => import('@/layouts/default/Default.vue'),
-    children: [
-      {
-        path: "/",
-        name: Login.name,
-        component: Login,
-      },
-      {
-        path: "/home",
-        name: HomeTabs.name,
-        component: HomeTabs,
-        children: [
-          {
-            path: "",
-            name: Inventory.name,
-            component: Inventory
-          },
-          {
-            path: "products",
-            name: Products.name,
-            component: Products
-          }
-        ]
-      }
-    ],
-  },
-]
+const routes = [{
+  path: '/', component: () => import('@/layouts/default/Default.vue'), children: [{
+    path: "/", name: Login.name, component: Login,
+  }, {
+    path: "/home", name: HomeTabs.name, component: HomeTabs, children: [{
+      path: "inventory-items", name: Inventory.name, component: Inventory
+    }, {
+      path: "products", name: ProductList.name, component: ProductList,
+    },]
+  }],
+},]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
+  history: createWebHistory(process.env.BASE_URL), routes,
 })
 router.beforeEach((to, from, next) => {
   const store = useAppStore()
--- eshop-ext-web/src/views/Home.vue
+++ eshop-ext-web/src/views/Home.vue
@@ -11,14 +11,26 @@
 </template>
 
 <script>
+import router from "@/router";
+
 export default {
   name: 'HomeView',
+  async mounted() {
+    const matchingTabs = this.tabs.filter(tab => this.$route.fullPath.includes(tab.route))
+    if (matchingTabs.length === 0) {
+      await router.push({path: this.tabs[0].route})
+    } else if (matchingTabs.length === 1) {
+      this.activeTab = matchingTabs[0].id
+    } else {
+      this.activeTab = matchingTabs[matchingTabs.length - 1].id
+    }
+  },
   data() {
     return {
-      activeTab: `/home`,
+      activeTab: `/home/inventory-items`,
       tabs: [
-        {id: 1, name: "Inventory", route: `/home`},
-        {id: 2, name: "Products", route: `/home/products`}
+        {id: 0, name: "Inventory", route: `/home/inventory-items`}, // Todo use named path
+        {id: 1, name: "Products", route: `/home/products`},
       ]
     };
   }
