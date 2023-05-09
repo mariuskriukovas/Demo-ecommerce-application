0a5141abbb6bfa8c8c47f20c0f84806c432a216b
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-05-09 11:09:14 +0300

    adding mdi icons;

--- eshop-ext-web/src/plugins/vuetify.js
+++ eshop-ext-web/src/plugins/vuetify.js
@@ -11,11 +11,20 @@ import 'vuetify/styles'
 // Composables
 import {createVuetify} from 'vuetify'
 import {VDataTable} from 'vuetify/labs/VDataTable'
+import {aliases, mdi} from 'vuetify/iconsets/mdi'
+
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   components: {
     VDataTable,
+  },
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    }
   },
   theme: {
     themes: {
