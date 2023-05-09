009d24cacf3f37b5a7914e5ef14d81ad50f4e679
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-05-09 11:07:42 +0300

    adding global go back;

--- eshop-ext-web/src/layouts/default/AppBar.vue
+++ eshop-ext-web/src/layouts/default/AppBar.vue
@@ -1,5 +1,12 @@
 <template>
   <v-app-bar color="primary" flat>
+    <v-icon
+      class="ml-2"
+      color="white-darken-2"
+      icon="mdi-arrow-left"
+      size="large"
+      @click="navigateBack"
+    ></v-icon>
     <v-app-bar-title>
       Demo UI for Marius ecommerce application
     </v-app-bar-title>
@@ -21,4 +28,9 @@ async function logout() {
   store.logout()
   await router.push({path: '/'})
 }
+
+async function navigateBack() {
+  await router.go(-1)
+}
+
 </script>
