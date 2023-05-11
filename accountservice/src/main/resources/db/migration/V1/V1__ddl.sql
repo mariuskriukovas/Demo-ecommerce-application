d4f909fff25ea3595b6bde3269340ab8a26306eb
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-05-11 10:43:25 +0300

    migrating to postgres;

--- accountservice/src/main/resources/db/migration/V1/V1__ddl.sql
+++ accountservice/src/main/resources/db/migration/V1/V1__ddl.sql
@@ -1,13 +1,7 @@
-CREATE TABLE shop_user
+CREATE TABLE account.shop_user
 (
-    id       IDENTITY PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
-    email    VARCHAR(100) NOT NULL
+    id       bigserial PRIMARY KEY,
+    username varchar(100) NOT NULL UNIQUE,
+    password varchar(100) NOT NULL,
+    email    varchar(100) NOT NULL UNIQUE
 );
-
-ALTER TABLE shop_user
-    ADD UNIQUE (username);
-
-ALTER TABLE shop_user
-    ADD UNIQUE (email);
