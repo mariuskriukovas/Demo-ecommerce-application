e311be8f7e0465ce77b457755357366b3beb8d63
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-05-12 16:44:04 +0300

    add ROLE checks for POST and PUT requests;

--- inventory/src/main/java/io/marius/demo/ecommerce/inventory/controller/ProductsController.java
+++ inventory/src/main/java/io/marius/demo/ecommerce/inventory/controller/ProductsController.java
@@ -3,6 +3,7 @@ package io.marius.demo.ecommerce.inventory.controller;
 import io.marius.demo.ecommerce.inventory.model.payload.ProductInput;
 import io.marius.demo.ecommerce.inventory.service.ProductService;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,11 +15,13 @@ public class ProductsController {
     this.productService = productService;
   }
 
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   @PostMapping(consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
   public String createProduct(@ModelAttribute ProductInput productInput) {
     return productService.createProduct(productInput);
   }
 
+  @PreAuthorize("hasRole('ROLE_ADMIN')")
   @PutMapping(
       path = "/{id}",
       consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
