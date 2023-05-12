1f44065301214254ef46007be5019c15b32f2717
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-05-12 16:43:08 +0300

    fix jwt decoder;

--- inventory/src/main/java/io/marius/demo/ecommerce/inventory/config/ResourceServerConfig.java
+++ inventory/src/main/java/io/marius/demo/ecommerce/inventory/config/ResourceServerConfig.java
@@ -1,28 +1,54 @@
 package io.marius.demo.ecommerce.inventory.config;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class ResourceServerConfig {
-    @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
+  @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+  private String issuerUri;
 
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+  @Bean
+  JwtDecoder jwtDecoder() {
+    return NimbusJwtDecoder.withJwkSetUri(this.issuerUri).build();
+  }
 
-        http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated()).httpBasic(withDefaults());
+  @Bean
+  public JwtAuthenticationConverter jwtAuthenticationConverter() {
+    JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
+        new JwtGrantedAuthoritiesConverter();
+    jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+    jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
 
-        http.oauth2ResourceServer().jwt();
-        return http.build();
-    }
+    JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+    jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+    return jwtAuthenticationConverter;
+  }
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.cors().and().csrf().disable();
+
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    http.authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
+        .httpBasic(withDefaults());
+
+    http.oauth2ResourceServer().jwt();
+    return http.build();
+  }
 }
