62ab2c5e7ccea0041f9261eb28551b433ac60edd
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-05-12 14:11:47 +0300

    update ddl to have role table;

--- accountservice/src/main/resources/db/migration/V1/V1__ddl.sql
+++ accountservice/src/main/resources/db/migration/V1/V1__ddl.sql
@@ -5,3 +5,12 @@ CREATE TABLE account.shop_user
     password varchar(100) NOT NULL,
     email    varchar(100) NOT NULL UNIQUE
 );
+
+CREATE TABLE account.role
+(
+    id      bigserial PRIMARY KEY,
+    user_id integer      NOT NULL,
+    role    varchar(100) NOT NULL,
+    CONSTRAINT fk_role_user_id FOREIGN KEY (user_id) REFERENCES account.shop_user (id),
+    UNIQUE (user_id, role)
+);
--- accountservice/src/main/resources/db/migration/V1/V2__insert_data.sql
+++ accountservice/src/main/resources/db/migration/V1/V2__insert_data.sql
@@ -5,3 +5,8 @@ VALUES ('userA', '$2a$10$XCBUcZPNaKakw5HRAjKk9OKo0iZkq0doEJUxRkXeyl/PqofG6z2LK',
        ('userB', '$2a$12$zc43GqFHIpHK.eUQN.14UuRoPmfpu6IQlcqzg4d5b9UrdYf0N4xz6', 'userB@gmail.com'),
        ('userC', '$2a$12$O8B2LDq7egMeD0usZvmHTeIpE4auaAyQbC2GKvn3jWJFBxDdr3N9C', 'userC@gmail.com');
 
+INSERT INTO account.role (user_id,
+                          role)
+VALUES ((select id from account.shop_user where username = 'userA'), 'USER'),
+       ((select id from account.shop_user where username = 'userB'), 'USER'),
+       ((select id from account.shop_user where username = 'userC'), 'ADMIN');
