06d8460cba5b4e66e8388910c06c4d01b3e9d98a
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-05-17 09:49:41 +0300

    migrating to commons converters;

--- inventory/src/main/java/io/marius/demo/ecommerce/inventory/config/WebConfig.java
+++ inventory/src/main/java/io/marius/demo/ecommerce/inventory/config/WebConfig.java
@@ -1,6 +1,8 @@
 package io.marius.demo.ecommerce.inventory.config;
 
-import io.marius.demo.ecommerce.inventory.objectconverter.PropertyInputConverter;
+import io.marius.demo.ecommerce.inventory.model.payload.PropertyInput;
+import io.marius.demo.ecommerce.persistence.api.converter.AtributeConverter;
+import io.marius.demo.ecommerce.persistence.api.converter.ClassifierViewConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,6 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
 
   @Override
   public void addFormatters(FormatterRegistry registry) {
+    registry.addConverter(new ClassifierViewConverter());
     registry.addConverter(new PropertyInputConverter());
+  }
+
+  private class PropertyInputConverter extends AtributeConverter<PropertyInput> {
+    public PropertyInputConverter() {
+      super(PropertyInput.class);
+    }
   }
 }
