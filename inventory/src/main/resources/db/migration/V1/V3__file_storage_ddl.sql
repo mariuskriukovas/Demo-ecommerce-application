aec356c2d6284fb40147ba519e9492df0dafa430
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-05-22 10:56:42 +0300

    adding uids to db model;

--- inventory/src/main/resources/db/migration/V1/V3__file_storage_ddl.sql
+++ inventory/src/main/resources/db/migration/V1/V3__file_storage_ddl.sql
@@ -1,6 +1,7 @@
 CREATE TABLE inventory.file_metadata
 (
     id             bigserial PRIMARY KEY,
+    uid            varchar(40)  NOT NULL UNIQUE,
     file_name      varchar(500) NOT NULL,
     file_extension varchar(10)  NOT NULL,
     file_key       varchar(100) NOT NULL UNIQUE,
@@ -10,8 +11,9 @@ CREATE TABLE inventory.file_metadata
 CREATE TABLE inventory.product_file
 (
     id         bigserial PRIMARY KEY,
-    product_id integer NOT NULL,
-    file_id    integer NOT NULL,
+    uid        varchar(40) NOT NULL UNIQUE,
+    product_id integer     NOT NULL,
+    file_id    integer     NOT NULL,
     UNIQUE (product_id, file_id),
     CONSTRAINT fk_product_file_product_id FOREIGN KEY (product_id) REFERENCES inventory.product (id),
     CONSTRAINT fk_product_file_file_metadata_id FOREIGN KEY (file_id) REFERENCES inventory.file_metadata (id)
