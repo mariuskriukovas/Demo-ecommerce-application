78a18febd72f3df4e23f3b15971373b495847e45
Marius.Kriukovas <marius.kriukovas@gmail.com>
2023-06-19 11:32:29 +0300

    fix typo;

--- accountservice/account-api/src/main/java/io/marius/demo/ecommerce/account/api/client/SessionClient.java
+++ accountservice/account-api/src/main/java/io/marius/demo/ecommerce/account/api/client/SessionClient.java
@@ -3,5 +3,5 @@ package io.marius.demo.ecommerce.account.api.client;
 import io.marius.demo.ecommerce.account.api.resource.SessionResource;
 import org.springframework.cloud.openfeign.FeignClient;
 
-@FeignClient(name = "classifiersClient", url = "${services.account.url}" + "/sessions")
+@FeignClient(name = "sessionClient", url = "${services.account.url}" + "/sessions")
 public interface SessionClient extends SessionResource {}
